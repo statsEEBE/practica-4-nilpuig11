e62d38a08e7ec4caef232799a326ed3e981765e2
nil.puig.biosca <nil.puig.biosca@auleseebe.upc.edu>
2024-10-31 11:38:18 +0100

    ...

--- Pregunta2.R
+++ Pregunta2.R
@@ -1,1 +1,10 @@
-
+#Pregunta 2
+#a)P(X>=13000) #lambda=1/E
+1-pexp(13000,1/10000)
+#apartat b) 0,8=F(t)
+qexp(0.8,1/10000)
+set.seed(85) #fixar semila (mateixos valors de experiments aleatoris)
+simulacion<-rexp(100,1/10000) #important guardar quan utilitzem set.seed
+mean(simulacion)
+median(simulacion)
+var(simulacion)
